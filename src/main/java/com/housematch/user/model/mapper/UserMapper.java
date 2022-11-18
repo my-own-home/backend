316be43d0d4f811d038a4e1997a74ee8e8202f01
sujung <hsj8518@gmail.com>
2022-11-18 15:05:55 +0900

    add: [mapper] User

---
+++ src/main/java/com/housematch/user/model/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.housematch.user.model.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.housematch.user.model.dto.UserDto;
+
+@Mapper
+public interface UserMapper {
+	
+	UserDto selectUser(String id);
+	
+	int insertUser(UserDto user);
+	
+	int updateUser(UserDto user);
+	
+	int deleteUser(String id);
+}
