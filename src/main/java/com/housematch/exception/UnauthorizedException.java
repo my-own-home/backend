68a7a8be4688c2c48f8ed0f29f376dcffe66e7ae
teamzamong <teamzamong@gmail.com>
2022-11-19 19:09:59 +0900

    feat: JWT login related code

---
+++ src/main/java/com/housematch/exception/UnauthorizedException.java
@@ -0,0 +1,9 @@
+package com.housematch.exception;
+
+public class UnauthorizedException extends RuntimeException {
+	private static final long serialVersionUID = 6727492779586566200L;
+
+	public UnauthorizedException() {
+		super("계정 권한이 유효하지 않습니다.\n다시 로그인을 해주세요.");
+	}
+}
