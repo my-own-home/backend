2da8de63e506c7a8ee52157f0315caf81ab2bb6e
teamzamong <teamzamong@gmail.com>
2022-11-20 15:25:38 +0900

    fix: add localhost to corsconfig

--- src/main/java/com/housematch/config/CorsConfig.java
+++ src/main/java/com/housematch/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-		.allowedOrigins("http://127.0.0.1:5500", "http://127.0.0.1:9000") // 라이브서버 주소
+		.allowedOrigins("http://127.0.0.1:5500", "http://127.0.0.1:9000", "http://localhost:3000", "http://localhost:9000") // 라이브서버 주소
         .allowedMethods("*")
 		.allowCredentials(true);
 	}
