d2a968bd4708235098847e7bac3e748ed20b89b1
teamzamong <teamzamong@gmail.com>
2022-11-22 03:29:55 +0900

    update: sql queries for aptdeal, aptdetail

--- src/main/java/com/housematch/house/controller/AptController.java
+++ src/main/java/com/housematch/house/controller/AptController.java
@@ -1,5 +1,8 @@
 package com.housematch.house.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,33 +33,36 @@ public class AptController {
 	@ApiOperation(value = "동별 아파트 목록 조회")
 	@GetMapping
 	public ResponseEntity<?> getAptListByDong(@RequestParam(required = true) String dongCode) {
-
 		return ResponseEntity.ok(aptInfoService.getAptListByDong(dongCode));
-
 	}
-	
+
 	@ApiOperation(value = "아파트 기본 정보 조회")
 	@GetMapping("/{aptCode}")
 	public ResponseEntity<?> getAptInfo(@PathVariable long aptCode) {
-
 		return ResponseEntity.ok(aptInfoService.getAptInfo(aptCode));
-
 	}
 
 	@ApiOperation(value = "아파트 상세 정보 조회")
 	@GetMapping("/{aptCode}/detail")
 	public ResponseEntity<?> getAptDetail(@PathVariable long aptCode) {
-
 		return ResponseEntity.ok(aptDetailService.getAptDetail(aptCode));
-
 	}
 
 	@ApiOperation(value = "아파트 실거래가 목록 조회")
 	@GetMapping("/{aptCode}/deals")
 	public ResponseEntity<?> getAptDealRecordList(@PathVariable long aptCode) {
-
 		return ResponseEntity.ok(aptDealRecordService.getAptDealRecordList(aptCode));
+	}
 
+	@ApiOperation(value = "아파트 실거래가 목록 조회")
+	@GetMapping("/{aptCode}/all")
+	public ResponseEntity<?> getAptAll(@PathVariable long aptCode) {
+		Map<String, Object> apt = new HashMap<String, Object>();
+		apt.put("basic", aptInfoService.getAptInfo(aptCode));
+		apt.put("detail", aptDetailService.getAptDetail(aptCode));
+//		apt.put("deals", aptDealRecordService.getAptDealRecordList(aptCode));		
+		
+		return ResponseEntity.ok(apt);
 	}
 
 }
